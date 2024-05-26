3184f8dae31fbec2ba24fcad1bc0ddb781eca92f
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-26 18:51:58 +0530

    new2

---
+++ new2_que1.py
@@ -0,0 +1,9 @@
+#wap to ask the user to enter names of their 3 favorite movies & store them in a list
+movies=[]
+mov1=input("enter the first movies name:")
+mov2=input("enter the second movies name:")
+mov3=input("enter the third movies name:")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
