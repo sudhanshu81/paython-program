ddd534d0d961ae2a81664e48426a1261dabd0cc1
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-26 14:46:01 +0530

    paython program

--- new.py
+++ new.py
@@ -20,7 +20,90 @@ else:
  print("broken light")"""
 
 
+#operator
+#arithmatic operator
+"""a=4
+b=9
+print(a+b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a*b)"""
 
+#relational operator
+
+"""a=4
+b=5
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)"""
+
+#logical operator
+
+"""a=30
+b=40
+print(not False)
+print(not(a>b))
+val1=False
+val2=False
+print("and operator:",val1 and val2)
+print("or operator:",(a==b)or (a>b))"""
+      
+
+#assignment operator
+"""a=6
+a+=5
+a-=5
+a*=5
+a/=5
+a**=5
+print("a:",a)
+print("a:",a)
+print("a:",a)
+print("a:",a)
+print("a:",a)"""
+
+#type conversion
+
+"""a=int("2")
+b=4.45
+
+print(type(a))
+print(a+b)"""
+
+#nasteing conditional statement
+"""
+age=23
+if(age<=78):
+    if(age<=18):
+        print("you can drive")
+    else:
+        print("dont drive")
+else:
+    print("cannot drive")"""
+
+#practice question
+
+
+
+#//wap to find the greatest of 3 numbers entered by the user.
+"""a=int(input("enter num1:"))
+b=int(input("enter the num2:"))
+c=int(input("enter the num3:"))
+if(a>=b and a>=c):
+    print("grestest number:",a)
+elif( b>=c):
+    print("greatest number is:",b)
+else:
+    print(" greatest number is:",c)"""
+
+# wap to check if a number is a multiple of 7 or not.
+
+"""a=int(input("enter the number:"))
+if(a%7==0):
+    print("number is multiple of 7:",a)
+"""
 
 
 
---
+++ que_1.py
@@ -0,0 +1,6 @@
+#// wap to check if a number entered by the user is odd or even.
+num=int(input("enter the number:"))
+if(num%2==0):
+     print("this odd number:",num)
+else:
+     print("this is even number:",num)
