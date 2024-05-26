c14748a4c63daa151b1a3f67fbb0f75d9b7b3c56
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-26 14:38:30 +0530

    conditional statement

--- new.py
+++ new.py
@@ -6,6 +6,19 @@ city=input("city:=")
 mo_no=input("mo_no:=")
 print("may name is:",name,"my age is:",age,"my city is:",city,"my contact no is:",mo_no)
 """
+#conditional statement
+"""light=input("light color:")
+if(light=="red"):
+    print("stop")
+
+elif(light=="green"):
+    print("go")
+
+elif(light=="yellow"):
+    print("please wait")
+else:
+ print("broken light")"""
+
 
 
 
