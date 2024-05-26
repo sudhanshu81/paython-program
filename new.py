dea73e72bebda34e5dd1fc640f410e37dd3d9183
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-26 14:34:28 +0530

    programing

---
+++ new.py
@@ -0,0 +1,18 @@
+#print("i am rahul mishra")
+#input given the user
+"""name=input("name:=")
+age=int(input("age:="))
+city=input("city:=")
+mo_no=input("mo_no:=")
+print("may name is:",name,"my age is:",age,"my city is:",city,"my contact no is:",mo_no)
+"""
+
+
+
+
+
+ 
+
+
+
+
