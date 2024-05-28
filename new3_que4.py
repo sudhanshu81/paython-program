34df958dd57566729e7fd99afa8f903b98724fb4
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-28 17:01:26 +0530

    new3_que3

---
+++ new3_que4.py
@@ -0,0 +1,18 @@
+
+
+marks={
+    
+}
+x=int(input("enter phy :"))
+marks.update({"phy":x})
+
+x=int(input("enter math :"))
+marks.update({"math":x})
+
+x=int(input("enter chem :"))
+marks.update({"chem":x})
+
+x=int(input("enter eng :"))
+marks.update({"eng":x})
+
+print(marks)
