0f63a91edb1c9714279ad869b666ad6b2a637293
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-28 14:39:50 +0530

    new3

---
+++ new3_que1.py
@@ -0,0 +1,15 @@
+# Create a dictionary print the student name and subject&marks.
+# using nasted dictionary concept.
+sudent ={
+    "name" : "rahul mishra",
+    "subjects" : {
+        "phy":98,
+        "eng":99,
+        "hindi":95,
+        "math":98,
+
+    }
+}
+print(sudent["name"])
+print(sudent["subjects"])
+print(sudent["subjects"]["math"])
