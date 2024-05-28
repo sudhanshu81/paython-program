7ef07168f657ed362092214dff4e60dd9bb6ce11
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-28 16:40:32 +0530

    new3

---
+++ new3_que3.py
@@ -0,0 +1,9 @@
+#you are given a list of subjects for students. Assume one classroom is required for 1
+#subject.how maney classrooms are needed by all students.
+
+student={
+    "paython","c++","java","c","sql","java","c++","c","paython","java","javascripts"
+    ,"c","paython"
+}
+print(student)
+print(len(student))
