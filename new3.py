6d1a7ed982ba797110af287164cb9d03cc3ce374
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-28 14:22:27 +0530

    new3

---
+++ new3.py
@@ -0,0 +1,17 @@
+#Dictionary in paython.
+
+info ={
+    "name":"rahulmishra",
+    "subject":["paython","c","c++","java"],
+    "topics":("dict","set"),
+    "age":21,
+    "is_adult":True,
+    12.99:94.5
+}
+
+print(info["name"])
+print(info["subject"])
+print(info["topics"])
+print(info["age"])
+print(info["is_adult"])
+print(info[12.99])
