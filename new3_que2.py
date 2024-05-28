f85367a03182709d98b7cf4d3a2deaae6d551e73
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-28 16:23:51 +0530

    new3

---
+++ new3_que2.py
@@ -0,0 +1,7 @@
+# Store following word meanings in a paython dictionary.
+
+dictionary={
+    "cat":"a small animal",
+    "table":["a piece of furniture","list of facts & figures"]
+}
+print(dictionary)
