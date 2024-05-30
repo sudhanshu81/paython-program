ecd0885dad492d1914fd73711c2237d244ae8aab
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-30 17:30:27 +0530

    new5

---
+++ new5.py
@@ -0,0 +1,13 @@
+# using range()
+"""for i in range(1,100,2):
+    print(i)"""
+
+"""n=int(input("enter number :"))
+for i in range(1,11):
+    print(n*i)"""
+
+#pass statement 
+
+for el in range(10):
+    pass
+print ("no elment")
---
+++ new5_que1.py
@@ -0,0 +1,8 @@
+
+#wap to find sum of first n numbers.(using for )
+n=5
+
+sum=0  
+for i in range(n,n+1):
+ sum += i
+print("total som", sum)
