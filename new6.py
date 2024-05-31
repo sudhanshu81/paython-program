385e8409638ce20967015cff479f1d1d51eb4f79
sudhanshu81 <145042748+sudhanshu81@users.noreply.github.com>
2024-05-31 15:22:15 +0530

    new6

---
+++ new6.py
@@ -0,0 +1,13 @@
+# recursion in python
+
+"""def show(n):
+    if(n==0):
+        return
+    print(n)
+    show(n-1)"""
+
+"""def fact(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return n * fact(n-1)"""
